e0058a3a9f4b87d58f99ebaa19e7c27b8f4498b0
russm3086 <git.hub@russmartinez.com>
2020-04-15 12:38:04 +0200

    1.4.5.8

--- OverWatch/src/com/ansys/cluster/monitor/data/SGE_DataConst.java
+++ OverWatch/src/com/ansys/cluster/monitor/data/SGE_DataConst.java
@@ -7,7 +7,7 @@ public final class SGE_DataConst {
 	}
 
 	public static String app_name = "OverWatch";
-	public static String app_version = "1.4.5.7";
+	public static String app_version = "1.4.5.8";
 	public static String app_version_regex = "(\\d+\\.)+(\\d)";
 	
 	
